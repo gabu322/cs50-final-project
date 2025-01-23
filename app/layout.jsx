f79ae759e6540b3ef71145bf2fab5703d7060983
gabu322 <gabrieldallorto@hotmail.com>
2025-01-23 05:01:18 -0300

    Updated background styling

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,9 @@
-import { Nunito } from "next/font/google";
+import { Quicksand } from "next/font/google";
 import "./globals.css";
 
-const nunito = Nunito({
+const quicksand = Quicksand({
    subsets: ["latin"],
-   weight: ['200', '300', '400', '500', '700', '900'],
+   weight: ['300', '400', '500', '700'],
 });
 
 export const metadata = {
@@ -11,9 +11,21 @@ export const metadata = {
    description: "A web application so you can save, search and share your recipes.",
 };
 
+const randomNumber = Math.floor(Math.random() * 3) + 1;
+const backgroundImage = `/background/${randomNumber}.png`;
+
 export default function RootLayout({ children }) {
    return <html lang="en">
-      <body className={nunito.className}>
+      <body
+         className={quicksand.className}
+         style={{
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+         }}
+      >
+         <div className="absolute top-0 left-0 w-full h-full bg-[#fff8ef] opacity-[97%] -z-10" />
          {children}
       </body>
    </html>;
