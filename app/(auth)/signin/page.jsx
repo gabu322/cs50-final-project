d4b939acf416b20692bdf6361a53d86b5a3c0e5a
gabu322 <gabrieldallorto@hotmail.com>
2025-01-23 01:22:16 -0300

    Updated sigin page to the new style

--- app/(auth)/signin/page.jsx
+++ app/(auth)/signin/page.jsx
@@ -36,15 +36,16 @@ export default function Page({ }) {
       }
    }
 
-   return <div className="flex flex-col justify-center items-center w-full gap-4 bg-white py-6 rounded-lg shadow-lg max-w-md">
+   return <div className="flex flex-col justify-center items-center w-full gap-4 bg-white py-6 rounded-3xl shadow-lg max-w-sm ">
       <h1 className="text-2xl">Login</h1>
 
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-4/5">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-full px-6">
          <Input
             name="email"
             label="Email"
             type="email"
             onChange={handleChange}
+            rounded
             required
          />
 
@@ -53,20 +54,28 @@ export default function Page({ }) {
             label="Senha"
             type="password"
             onChange={handleChange}
+            rounded
             required
          />
 
          <Button
             type="submit"
+            bgColor={"bg-orange-300"}
+            hoverColor={"hover:bg-orange-400"}
             text={"Entrar"}
+            rounded
          />
+
          <hr />
 
          <p className="text-left">Ainda não possui cadastro?</p>
          <Button
             className="w-full"
             text={"Cadastrar"}
+            bgColor={"bg-orange-300"}
+            hoverColor={"hover:bg-orange-400"}
             href={"/signup"}
+            rounded
          />
       </form>
    </div>;
