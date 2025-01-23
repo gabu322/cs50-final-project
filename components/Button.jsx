82b10e6c3fa2ca0d283580663705c8d55fd71c11
gabu322 <gabrieldallorto@hotmail.com>
2025-01-23 01:13:09 -0300

    Updated Button component to have better support for coloring and href

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,71 +1,41 @@
 import Link from 'next/link';
 
+const bgColors = {
+   blue: { bg: "bg-blue-600", hover: "bg-blue-700" },
+   red: { bg: "bg-red-500", hover: "bg-red-600" },
+   green: { bg: "bg-green-600", hover: "bg-green-700" },
+   yellow: { bg: "bg-yellow-300", hover: "bg-yellow-400" },
+   orange: { bg: "bg-orange-400", hover: "bg-orange-500" },
+   black: { bg: "bg-black", hover: "bg-gray-800" }
+}
+
 export default function Button({
-   children,
-   href,
+   className,
    type,
    onClick,
-   className,
-   transparent,
-   blank,
-   textColor = "text-white",
-   color = "bg-blue-600 hover:bg-blue-700",
+   color = "blue",
+   bgColor,
+   hoverColor,
    text,
+   textColor = "text-white",
+   href,
+   blank = "",
+   children,
+   rounded,
    disabled
 }) {
+   const colors = bgColors[color];
+   const bg_color = bgColor || colors.bg;
+   const hover_color = hoverColor || colors.hover;
 
-   if (textColor == "black") {
-      textColor = " text-black";
-   } else if (textColor == "red") {
-      textColor = " text-red-500";
-   } else if (textColor == "green") {
-      textColor = " text-green-500";
-   } else if (textColor == "yellow") {
-      textColor = " text-yellow-500";
-   } else if (textColor == "gray") {
-      textColor = " text-gray-500";
-   } else if (textColor == "pink") {
-      textColor = " text-pink-500";
-   }
-
-   if (transparent || color == "transparent") {
-      color = "bg-transparent";
-      textColor = " text-black font-bold";
-   } else if (color == "red") {
-      color = "bg-red-500 hover:bg-red-600";
-   } else if (color == "blue") {
-      color = "bg-blue-500 hover:bg-blue-600";
-   } else if (color == "green") {
-      color = "bg-green-500 hover:bg-green-600";
-   } else if (color == "yellow") {
-      color = "bg-yellow-500 hover:bg-yellow-600";
-   } else if (color == "gray") {
-      color = "bg-gray-500 hover:bg-gray-600";
-   } else if (color == "pink") {
-      color = "bg-pink-500 hover:bg-pink-600";
-   } else if (color == "white") {
-      color = "bg-white hover:bg-gray-100";
-   } else if (color == "black") {
-      color = "bg-black hover:bg-gray-800";
-   }
-
-   const buttonContent = <button
+   return <button
       type={type ?? "button"}
-      className={`px-3 rounded drop-shadow transition-all duration-200 cursor-pointer font-semibold text-sm flex-r-2 items-center justify-center ${color} ${textColor} ${className} h-10`}
+      className={`flex items-center justify-center relative h-10 px-3 shadow-sm transition-all duration-200 cursor-pointer font-semibold text-sm ${textColor} ${className} ${bg_color} ${hover_color} ${rounded ? "rounded-full" : "rounded"}`}
       onClick={onClick}
       disabled={disabled}
    >
       {children || text}
+
+      {href && <Link href={href} target={blank} className="absolute w-full h-full" />}
    </button>;
-
-   if (href) {
-      return (
-
-         <Link href={href} target={blank ? "_blank" : ""} className={className}>
-            {buttonContent}
-         </Link>
-      );
-   }
-
-   return buttonContent;
 }
