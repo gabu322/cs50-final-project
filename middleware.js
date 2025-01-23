287bc4755461bd847b8681f20398bf44a2121f64
gabu322 <gabrieldallorto@hotmail.com>
2025-01-23 05:04:28 -0300

    Added background route to middleware

--- middleware.js
+++ middleware.js
@@ -13,6 +13,6 @@ export async function middleware(request) {
 
 export const config = {
    matcher: [
-      "/((?!_next|api/auth|signin|signup).*)",
+      "/((?!_next|api/auth|signin|signup|background).*)",
    ],
 };
