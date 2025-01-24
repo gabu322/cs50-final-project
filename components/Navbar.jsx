93d45d51b40ef45ac4cd133ed3359226806d1461
gabu322 <gabrieldallorto@hotmail.com>
2025-01-24 04:41:35 -0300

    Updated Navbar to new styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
       console.log(session);
    }, [session]);
 
-   return <nav className="bg-gray-800 text-white p-4 fixed top-0 left-0 w-full z-10 flex flex-row justify-between items-center">
+   return <nav className="bg-orange-500 text-white py-4 px-10 fixed top-0 left-0 w-full z-10 flex flex-row justify-between items-center">
       <h2 className="text-2xl">CookBook</h2>
 
       <Select
@@ -23,6 +23,6 @@ export default function Navbar() {
          rounded
       />
 
-      <h3 className="bg-gray-700 p-2 px-4 rounded-full text-lg">{session?.user.email.split("@")[0]}</h3>
+      {/* <h3 className="bg-orange-300 p-2 px-4 rounded-full text-lg">{session?.user.email.split("@")[0]}</h3> */}
    </nav>;
 }
