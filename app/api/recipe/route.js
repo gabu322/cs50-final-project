e7e8c545fa580a7c06be7d5ee56ef23d8eeea269
gabu322 <gabrieldallorto@hotmail.com>
2025-01-28 02:44:11 -0300

    Added GET api route for recipes

--- app/api/recipe/route.js
+++ app/api/recipe/route.js
@@ -3,11 +3,32 @@ import { prisma } from "@/prisma/client";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-export async function POST(req) {
+export async function GET(req) {
    try {
       const session = await getServerSession(authOptions);
 
-      if (!session || !session.user || !session.user.id) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+      const recipes = await prisma.recipe.findMany({
+         where: {
+            OR: [
+               { userId: session.user.id },
+               { private: false },
+            ]
+         },
+         include: {
+            ingredients: true,
+            instructions: true,
+         },
+      });
+
+      return NextResponse.json(recipes, { status: 200 });
+   } catch (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+   }
+}
+
+export async function POST(req) {
+   try {
+      const session = await getServerSession(authOptions);
 
       const recipeData = await req.json();
 
@@ -15,15 +36,20 @@ export async function POST(req) {
          console.log("here1")
          const recipe = await tx.recipe.create({
             data: {
-               title: recipeData.title,
+               title: recipeData.name,
                description: recipeData.description,
                image: recipeData.image || null,
-               userId: session.user.id,
+               private: recipeData.private,
+               prepTime: +recipeData.prepTime,
+
+               user: { connect: { id: session.user.id } },
+
+               category: { connect: { id: recipeData.category } },
 
                ingredients: {
                   create: recipeData.ingredients.map((ingredient) => ({
                      name: ingredient.name,
-                     quantity: ingredient.quantity,
+                     quantity: +ingredient.quantity,
                      unit: ingredient.unit,
                   })),
                },
@@ -31,7 +57,7 @@ export async function POST(req) {
                instructions: {
                   create: recipeData.instructions.map((instruction) => ({
                      content: instruction.content,
-                     step: instruction.step,
+                     step: +instruction.step,
                   })),
                },
             },
